95217429b4e8c53c9279885f28568e66c875ff3f
Lucas <lucas.teixeira@calindra.com.br>
2021-04-21 23:53:36 -0300

    first commit

---
+++ precache-manifest.1a84a73c4f2adf2462f5b998068b5286.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cce19fabc0a3dbe043c28dfb1f6920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "966278d604e8d6cb344d",
+    "url": "/static/js/2.01d62af1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.01d62af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167145afd4a136ff066e",
+    "url": "/static/js/main.b1a04442.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
